7e786790569394e1f9a319316d5650fbf78dcc35
ZHEYTHAN <andresgam31@gmail.com>
2024-12-27 16:23:46 -0500

    actualizacion

--- tres_en_linea/src/App.jsx
+++ tres_en_linea/src/App.jsx
@@ -5,8 +5,18 @@ const TURNS = {
   o: "O",
 };
 
-const Square = ({ children, updateBoard, index }) => {
-  return <div className="square">{children}</div>;
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+
+  const handleClick = () => {
+    updateBoard(index);
+  };
+
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
 };
 
 function App() {
@@ -14,13 +24,31 @@ function App() {
 
   const [turn, setTurn] = useState(TURNS.x);
 
+  const updateBoard = (index) => {
+    const newBoard = [...board];
+    newBoard[index] = turn;
+
+    setBoard(newBoard);
+
+    const newTurn = turn === TURNS.x ? TURNS.o : TURNS.x;
+    setTurn(newTurn);
+  };
+
   return (
     <main className="board">
       <h1>Tres en Línea</h1>
       <section className="game">
         {board.map((_, index) => {
-          return <Square key={index} index={index}></Square>;
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {board[index]}
+            </Square>
+          );
         })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.x}>{TURNS.x}</Square>
+        <Square isSelected={turn === TURNS.o}>{TURNS.o}</Square>
       </section>
     </main>
   );
